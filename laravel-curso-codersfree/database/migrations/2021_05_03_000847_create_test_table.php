f21582ef416669a0aa338030f4e2f48475c36a03
diegofh21 <dfuenmayor76@gmail.com>
2021-05-11 17:29:49 -0400

    initial commit

---
+++ laravel-curso-codersfree/database/migrations/2021_05_03_000847_create_test_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->foreignId('user_id')
+            ->constrained('users')
+            ->onDelete('cascade');
+
+            // $table->unsignedSmallInteger('id_tipo_usuarios');
+            // $table->foreign('id_tipo_usuarios')->references('id_tipo_usuarios')->on('tipo_usuarios')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test');
+    }
+}
